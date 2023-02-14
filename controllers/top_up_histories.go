31e39643845cf1faed2e400a637c2d7192e22359
hdkef <hdkef11@gmail.com>
2023-02-14 09:34:47 +0700

    add top up history

--- controllers/top_up_histories.go
+++ controllers/top_up_histories.go
@@ -2,15 +2,49 @@ package controllers
 
 import (
 	"database/sql"
+	"fmt"
 	"project/models"
 )
 
 func TopUpHistories(db *sql.DB, user models.User) int {
-	//pilih jangka waktu
+	//pilih jangka waktu dalam hari
+	var day int
+	fmt.Println("input jangka waktu (dalam hari) :")
+	fmt.Scanln(&day)
 
-	//select all data from top_up_histories
+	// //select all data from top_up_histories
+	rows, err := db.Query("SELECT date, amount, info FROM top_up_histories WHERE user_id = ? AND datediff(now(),date) <= ?", user.ID, day)
+	if err != nil {
+		fmt.Println("gagal menampilkan riwayat top up")
+		return -1
+	}
+
+	topup := []models.TopUpHistories{}
+
+	for rows.Next() {
+		var tmp models.TopUpHistories
+		err = rows.Scan(&tmp.Date, &tmp.Amount, &tmp.Info)
+		if err != nil {
+			fmt.Println("gagal menampilkan riwayat top up")
+			return -1
+		}
+		topup = append(topup, tmp)
+	}
 
 	//tampilkan data
+	for i, v := range topup {
+		fmt.Printf("No :\t%d\n", i+1)
+		fmt.Printf("Tanggal :\t%s\n", v.Date.String())
+		fmt.Printf("Jumlah :\t%.2f\n", v.Amount)
+		fmt.Printf("Info :\t%s\n", v.Info)
+	}
 
-	return -1
+	fmt.Println("pilih menu\n1.menu utama\n2.exit")
+	var opsi int
+	fmt.Scanln(&opsi)
+	if opsi == 1 {
+		return -1
+	} else {
+		return 9
+	}
 }
