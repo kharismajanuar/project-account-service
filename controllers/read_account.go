a4f7df0c027de3e24598b2d7c0b22f6987c49883
hdkef <hdkef11@gmail.com>
2023-02-15 09:51:54 +0700

    query info akun realtime

--- controllers/read_account.go
+++ controllers/read_account.go
@@ -6,8 +6,16 @@ import (
 	"project/models"
 )
 
-func ReadAccount(db *sql.DB, user models.User) int {
+func ReadAccount(db *sql.DB, ID int) int {
 	//tampilkan data pribadi
+	var user models.User
+	err := db.QueryRow("SELECT name, phone, date_of_birth, sex FROM users WHERE id = ?", ID).
+		Scan(&user.Name, &user.Phone, &user.DateOfBirth, &user.Sex)
+	if err != nil {
+		fmt.Println("tidak dapat menampilkan info akun")
+		return -1
+	}
+
 	fmt.Println("Nomor telepon\t:", user.Phone)
 	fmt.Println("Nama\t:", user.Name)
 	fmt.Println("Tanggal Lahir\t:", user.DateOfBirth)
@@ -15,7 +23,7 @@ func ReadAccount(db *sql.DB, user models.User) int {
 
 	//tampilkan saldo
 	var saldo float64
-	err := db.QueryRow("SELECT balance FROM balances WHERE user_id = ?", user.ID).Scan(&saldo)
+	err = db.QueryRow("SELECT balance FROM balances WHERE user_id = ?", user.ID).Scan(&saldo)
 	if err != nil {
 		fmt.Println("tidak dapat menampilkan saldo")
 		return -1
